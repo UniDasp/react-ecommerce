3a1f3f7948fd982fb7902ffbc0826fb64690d3d3
Diego <174396530+UniDasp@users.noreply.github.com>
2025-10-17 20:36:54 -0300

    Actualizar vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/react-ecommerce/',
   test: {
     environment: 'jsdom',
     setupFiles: './vitest.setup.js',
